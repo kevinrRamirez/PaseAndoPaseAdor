1e8d8bf914b41dfefa04138f739675fd6c490ee9
kevinrRamirez <57425588+kevinrRamirez@users.noreply.github.com>
2019-11-18 21:29:32 -0600

    Initial commit PaseAndo

---
+++ app/src/main/java/com/example/paseandopaseador/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.paseandopaseador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ctrlBotonIngresar(View view)
+    {
+        Intent intent = new Intent(view.getContext(), PaseAndoNavi.class);
+        startActivity(intent);;
+    }
+
+}
