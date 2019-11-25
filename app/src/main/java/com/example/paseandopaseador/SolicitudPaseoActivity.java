5addbee543b6fbda95baa6c33d01e51b4159bbdb
kevinrRamirez <57425588+kevinrRamirez@users.noreply.github.com>
2019-11-24 22:36:46 -0600

    Iconos agregados

---
+++ app/src/main/java/com/example/paseandopaseador/SolicitudPaseoActivity.java
@@ -0,0 +1,14 @@
+package com.example.paseandopaseador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SolicitudPaseoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitud_paseo);
+    }
+}
