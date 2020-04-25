1c596229d87e171219d27724b7b6131b9b5ac6fd
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2020-04-24 20:53:44 -0500

    New Activity: MiServicioActivity

---
+++ app/src/main/java/com/example/paseandopaseador/MiServicioActivity.java
@@ -0,0 +1,14 @@
+package com.example.paseandopaseador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MiServicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mi_servicio);
+    }
+}
